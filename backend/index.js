d6254eeabc601f61666a349ccec4f094319235ca
Theran Brigowatz <theran.brigowatz@gmail.com>
2019-04-10 02:37:42 +0900

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,6 @@ var cors = require('cors');
 
 dotenv.config();
 
-app.use(cors());
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,7 +27,7 @@ const gameRequest =
 	'name,summary,platforms.name,genres.name,involved_companies.company.name,cover.image_id,release_dates.y,screenshots.image_id,rating';
 
 // Random Game Route
-app.get('/api/random', async (req, res) => {
+app.get('/api/random', cors(), async (req, res) => {
 	const platform = req.query.platform || 18;
 	const randomRating = Math.floor(Math.random() * 100);
 
@@ -50,7 +48,7 @@ app.get('/api/random', async (req, res) => {
 });
 
 // Route with Game ID
-app.get('/api/game/:gameId/:platform', async (req, res) => {
+app.get('/api/game/:gameId/:platform', cors(), async (req, res) => {
 	const gameId = req.params.gameId;
 	try {
 		const response = await apicalypse(requestOptions)
